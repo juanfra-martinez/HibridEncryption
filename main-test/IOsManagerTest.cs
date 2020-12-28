0a150d34e435bc5e9b4242a043cbbb1f250598d1
jfmartinez <jfmartinez@vivaldi.net>
2020-12-28 16:07:56 +0100

    Added IOsManager.cs

---
+++ main-test/IOsManagerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using main;
+using Xunit;
+
+
+namespace main_test
+{
+    public class IOsManagerTest
+    {
+        public IOsManagerTest()
+        {
+        }
+
+        [Fact]
+        public void ShouldGetScreenShot()
+        {
+            IOsManager manager = new IOsManager();
+            var image = manager.GetScreenShot();
+            var imagePath = "test/screenshot.png";
+
+            byte[] iBytes = Convert.FromBase64String(image);
+            File.WriteAllBytes(imagePath, iBytes);
+
+            Assert.True(File.Exists(imagePath));
+        }
+    }
+}
---
+++ main/IOsManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace main
+{
+    public class IOsManager
+    {
+        public IOsManager()
+        {
+        }
+
+        public string GetScreenShot()
+        {
+            try
+            {
+                var screenshot = Pranas.ScreenshotCapture.TakeScreenshot();
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    screenshot.Save(ms, screenshot.RawFormat);
+                    byte[] imageBytes = ms.ToArray();
+                    return Convert.ToBase64String(imageBytes);
+                }
+            } catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
